67730a26018acde5e1a3c52e173034017c266d1f
aamilham <aamilham13@gmail.com>
2023-08-21 09:28:34 +0700

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,102 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Rahmad Ilham Sani",
+  jobDescription: "Junior Web Developer",
+  about: `Hello everyone, I'm Rahmad Ilham Sani, IPB University Second Year Computer Science students. Currently I'm learning Coding and Design. I have a strong interest in Information & Technology, especially Front-end Developer and Graphic Designer.
+  `,
+
+  experience: [
+    {
+      name: "InterNative Labs, LLC",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "Apr 2022",
+      endDate: "Present",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+
+    {
+      name: "Coupon Approve (Remote)",
+      location: "Amsterdam, North Holland, Netherlands",
+      startDate: "Nov 2020",
+      endDate: "Apr 2022",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "Interns Pakistan",
+      location: "Pakistan",
+      startDate: "Oct 2020",
+      endDate: "Nov 2020",
+      description: [
+        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "IPB University",
+      location: "Dramaga, Bogor, Jawa Barat",
+      startDate: "2021",
+      endDate: "Now",
+      description: [
+        "Third Year Computer Science Student at IPB University",
+      ],
+    },
+    {
+      name: "SMAN 1 BUKITTINGGI",
+      location: "Bukittinggi, West Sumatera",
+      startDate: "2018",
+      endDate: "2021",
+      description: ["HighSchool"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/aamilham",
+    email: "aamilham13@gmail.com",
+    linkedin: "https://www.linkedin.com/in/rahmad-ilham-sani-433247233/",
+  },
+
+  projects: [
+    {
+      title: "GDSC Movie Web Projects",
+      isFeatured: true,
+      thumbnail: "/assets/images/Gdsc-Web-Aam.png",
+      githubUrl: "https://github.com/aamilham/Rahmad-Ilham-Sani-Web-Movies",
+      liveUrl: "https://aamilham.github.io/Rahmad-Ilham-Sani-Web-Movies",
+    },
+    {
+      title: "IWDC Vacation Web Projects",
+      isFeatured: true,
+      thumbnail: "/assets/images/Iwdc-Web-Aam.png",
+      githubUrl: "https://github.com/aamilham/rahmad-ilham-sani-iwdc",
+      liveUrl: "https://aamilham.github.io/rahmad-ilham-sani-iwdc",
+    },
+    {
+      title: "Basis Data Projects Web",
+      isFeatured: true,
+      thumbnail: "/assets/images/GetNom-Project-Basdat.png",
+      githubUrl: "https://github.com/aamilham/ProjectBasdat",
+      liveUrl: "https://github.com/aamilham/ProjectBasdat",
+    },
+    {
+      title: "Rekayasa Perangkat Lunak Projects Web",
+      isFeatured: true,
+      thumbnail: "/assets/images/KontrakanID-Project-RPL.png",
+      githubUrl: "https://github.com/aamilham/KontrakanID",
+      liveUrl: "https://github.com/aamilham/KontrakanID",
+    },
+  ],
+};
